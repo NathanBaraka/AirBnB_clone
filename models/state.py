18b648bf3f4943130c5439a31a79bc30aa07981c
NathanBaraka <nathanbaraka23@gmail.com>
2024-03-11 11:02:37 -0700

    airbnb_clone

---
+++ models/state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This Module is for the State class.
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    This represents a state.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+
+    name = ""
